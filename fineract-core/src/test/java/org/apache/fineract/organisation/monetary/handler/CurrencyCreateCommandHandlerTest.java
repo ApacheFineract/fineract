44be273bb3ff8d671b56b5171797e6d931658d44
kapil-panchal <kapil.panchal@nouvelletechnology.com.au>
2025-07-02 14:56:26 +0530

    [unit-test] fineract core added

--- fineract-core/src/test/java/org/apache/fineract/organisation/monetary/handler/CurrencyCreateCommandHandlerTest.java
+++ fineract-core/src/test/java/org/apache/fineract/organisation/monetary/handler/CurrencyCreateCommandHandlerTest.java
@@ -1,25 +1,64 @@
 package org.apache.fineract.organisation.monetary.handler;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.UUID;
+
+import org.apache.fineract.command.core.Command;
+import org.apache.fineract.infrastructure.core.service.DateUtils;
+import org.apache.fineract.organisation.monetary.data.CurrencyData;
+import org.apache.fineract.organisation.monetary.domain.CreateCurrency;
+import org.apache.fineract.organisation.monetary.service.CurrencyWritePlatformService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ExtendWith(MockitoExtension.class)
 class CurrencyCreateCommandHandlerTest {
-	
+
+	private List<CurrencyData> currenciesGood;
+
 	@InjectMocks
 	private CurrencyCreateCommandHandler underTest;
-	
+
+	@Mock
+	private CurrencyWritePlatformService writePlatformService;
+
 	@BeforeEach
 	void setUp() throws Exception {
-		
+		ObjectMapper objectMapper = new ObjectMapper();
+		InputStream inputStream = getClass().getClassLoader()
+				.getResourceAsStream("test-resources/currenciesGoodData.json");
+
+		currenciesGood = objectMapper.readValue(inputStream, new TypeReference<List<CurrencyData>>() {
+		});
 	}
 
 	@Test
-	void testHandle() {
-		System.out.println("Create Command Unit Test Case");
+	void testHandleHappyPath() {
+		System.out.println("Unit Testing Handle");
+
+		for (CurrencyData element : currenciesGood) {
+			Command<CurrencyData> command = new Command<>();
+			command.setId(UUID.randomUUID());
+			command.setCreatedAt(DateUtils.getAuditOffsetDateTime());
+			command.setPayload(element);
+
+			Mockito.when(writePlatformService.createCurrency(command.getPayload())).thenReturn(element);
+
+			System.out.println("Currently Testing: " + element.getCode() + " Currency.");
+			System.out.println();
+			assertThat(underTest.handle(command)).isEqualTo(element);
+		}
 	}
 
 }
